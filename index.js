c7147681f6f2d8c9cac33ab310440ea8263e7baa
agurung01 <gurungaatish1@gmail.com>
2018-07-10 18:34:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function shout(string) {
   return string.toUpperCase()
-};
+}
 
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string.toUpperCase())
 }
@@ -15,10 +16,16 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can\'t hear you!";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+    return "I can't hear you!"
   }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
